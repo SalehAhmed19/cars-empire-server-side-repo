7007ee3ddd440fb3b42e3a62c2120c26c5659648
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-01 11:23:14 +0600

    data api created

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 // Zv3pa1mUsCSHyVCe
 // carsEmpire
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 const app = express();
+require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzir2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const carsCollection = client.db("carsEmpire").collection("cars");
+    app.get("/cars", async (req, res) => {
+      const query = {};
+      const cursor = carsCollection.find(query);
+      const cars = await cursor.toArray();
+      res.send(cars);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
