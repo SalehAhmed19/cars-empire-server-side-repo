29e2c1a6a6e741519e54fc5b66791e4f509158a7
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-01 10:27:41 +0600

    server basic set-up ready

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.listen(port, () => {
+  console.log("Listening to port: ", port);
+});
