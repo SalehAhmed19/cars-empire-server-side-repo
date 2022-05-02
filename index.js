271a43603ce649cc52e07db86ff26273f1797ca5
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-02 15:10:32 +0600

    single item delete api created

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
       const car = await carsCollection.findOne(query);
       res.send(car);
     });
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
